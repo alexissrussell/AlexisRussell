e77bcef3618437130c3fdcfa3e8ae8b7b6b30da0
Alexis Russell <alexissrussell@users.noreply.github.com>
2017-05-06 00:24:32 -0700

    fixed header

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ $(function () {            
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');                    
             if (target.length) {                        
                 $('html, body').animate({                            
-                    scrollTop: target.offset().top - 50                        
+                    scrollTop: target.offset().top - 150                        
                 }, 500);                        
                 return false;                    
             }                
         }            
     });        
+});
+
+// shrinking header
+
+$(window).scroll(function(){
+    
+   if ($(document).scrollTop()>500){
+       $('#header2').css('display','block');   
+   } else{
+       $('#header2').css('display','none');
+   }
+    
 });
