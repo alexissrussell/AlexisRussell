558e181763b957056c38b1055df2435fb87da24f
Alexis Russell <alexissrussell@users.noreply.github.com>
2017-05-06 10:58:12 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ $(function () {            
 
 // shrinking header
 
-$(window).scroll(function(){
+$(document).scroll(function(){
     
    if ($(document).scrollTop()>500){
        $('#header2').css('display','block');   
@@ -24,3 +24,51 @@ $(window).scroll(function(){
    }
     
 });
+
+
+
+
+//page arrows
+
+
+var leftA = document.getElementById("leftArrow");
+var downA =  document.getElementById("downArrow");
+var rightA = document.getElementById("rightArrow");
+
+
+leftA.addEventListener("click",function(){
+   
+    console.log("About Page");
+    window.open("about.html","_self");
+    
+});
+
+
+// Contact Form 
+
+
+$('#submit').click(function(ev){
+    console.log("Submit Button")
+    ev.preventDefault();
+    $('#popup').css('display','block');
+    $('#popup2').css('display','block');
+    $('#exitBut').css('display','block');
+    
+    var input = document.getElementById("name");
+    var span = document.getElementById("span_name")
+    
+        span.innerHTML = input.value;
+       
+});
+
+$('#exitBut').click(function(ev){
+    console.log("Submit Button")
+    ev.preventDefault();
+    $('#popup').css('display','none');
+    $('#popup2').css('display','none');
+    $('#exitBut').css('display','none');
+    
+});
+
+
+
